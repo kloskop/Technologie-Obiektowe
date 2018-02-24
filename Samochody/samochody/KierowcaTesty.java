33b3990613b9477e3bb42d51ed0449e00af07105
student12 <student12@S7N-PROGLANG-42.pcoip.ki.agh.edu.pl>
2018-02-24 14:27:53 +0100

    Tests for Kierowca class

--- Samochody/samochody/KierowcaTesty.java
+++ Samochody/samochody/KierowcaTesty.java
@@ -1,15 +1,20 @@
 package samochody;
+import java.util.ArrayList;
+import java.util.List;
+
 import org.junit.Test;
 import org.junit.Before;
+
 import static org.junit.Assert.*;
 
 public class KierowcaTesty {
 	
 	Kierowca kierowca;
-	
+	List<Samochod> samochody;
 	@Before
 	public void inicjujTesty()
 	{
+		samochody= new ArrayList<Samochod>();
 		kierowca = new Kierowca(3843, "John", "Doe");
 	}
 	
@@ -17,6 +22,21 @@ public class KierowcaTesty {
 	public void testDaneKierowcy()
 	{
 		assertEquals(kierowca.getImieINazwisko(),"John Doe");
+	}
+	
+	@Test
+	public void testowanieZnajdowaniaJednegoSamochodu()
+	{
+		Samochod samochod1=new Samochod("KGR", 10000, 10);
+		kierowca.dodajSamochod(samochod1);
+		assertEquals(kierowca.znajdzSamochod("KGR").getRejestracja(),"KGR");
+	}
+	
+	
+	@Test
+	public void testowanieGetPesel()
+	{
+		assertEquals(kierowca.getPesel(),3843);
 	}
 
 }
